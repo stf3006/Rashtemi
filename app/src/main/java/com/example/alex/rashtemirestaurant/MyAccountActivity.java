960fb92b05db7f9c82dfbfd5ed1991673ca74f51
Alex <stefan.alexandru014@yahoo.com>
2016-11-23 19:55:36 +0200

    meniu principal pagina login pagina contact pagina new account

--- app/src/main/java/com/example/alex/rashtemirestaurant/MyAccountActivity.java
+++ app/src/main/java/com/example/alex/rashtemirestaurant/MyAccountActivity.java
@@ -1,7 +1,10 @@
 package com.example.alex.rashtemirestaurant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyAccountActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MyAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
 
-        
+        Button signupBtn = (Button)findViewById(R.id.signup_btn);
+        signupBtn.setOnClickListener(new View.OnClickListener() { // TRECEREA DE LA O ACTIVITATE LA ALTA PRIN BUTON
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MyAccountActivity.this, SignUpActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
