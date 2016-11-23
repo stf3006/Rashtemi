f97c95a152a310b4405dc544eafcb239681cf260
Alex <stefan.alexandru014@yahoo.com>
2016-11-23 19:11:58 +0200

    meniu principal pagina login pagina contact

--- app/src/main/java/com/example/alex/rashtemirestaurant/MainActivity.java
+++ app/src/main/java/com/example/alex/rashtemirestaurant/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alex.rashtemirestaurant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button contactBtn = (Button)findViewById(R.id.contact_button);
+        contactBtn.setOnClickListener(new View.OnClickListener() { // TRECEREA DE LA O ACTIVITATE LA ALTA PRIN BUTON
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button myaccBtn = (Button)findViewById(R.id.myacc_button);
+        myaccBtn.setOnClickListener(new View.OnClickListener() { // TRECEREA DE LA O ACTIVITATE LA ALTA PRIN BUTON
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, MyAccountActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/alex/rashtemirestaurant/MyAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.alex.rashtemirestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+
+        
+    }
+}
