33d7d00949983aeeb89e20e7b6e43dd23ffadd79
Alex <stefan.alexandru014@yahoo.com>
2016-11-28 19:22:38 +0200

    preluare date myaccout (login / signup) mesaje de verificarea datelor

--- app/src/main/java/com/example/alex/rashtemirestaurant/MyAccountActivity.java
+++ app/src/main/java/com/example/alex/rashtemirestaurant/MyAccountActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MyAccountActivity extends AppCompatActivity {
 
@@ -19,6 +21,24 @@ public class MyAccountActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent1 = new Intent(MyAccountActivity.this, SignUpActivity.class);
                 startActivity(intent1);
+            }
+        });
+
+        Button loginBtn = (Button)findViewById(R.id.login_btn);
+        final EditText ed1 = (EditText)findViewById(R.id.username_login_et);
+       final  EditText ed2 = (EditText)findViewById(R.id.password_login_et);
+        Boolean logat = false;
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = ed1.getText().toString();
+                String s2 = ed2.getText().toString();
+
+                if(s1.equals("") || s1.equals("")){
+                    Toast.makeText(getApplicationContext(),"Please, insert data!",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/alex/rashtemirestaurant/SignUpActivity.java
+++ app/src/main/java/com/example/alex/rashtemirestaurant/SignUpActivity.java
@@ -2,6 +2,10 @@ package com.example.alex.rashtemirestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button createNewAccount = (Button)findViewById(R.id.createAccount_button);
+        final EditText editTextName = (EditText)findViewById(R.id.name_signup_et);
+        final EditText editTextPhone = (EditText)findViewById(R.id.phone_signup_et);
+        final EditText editTextUsername = (EditText)findViewById(R.id.username_signup_et);
+        final EditText editTextPassword = (EditText)findViewById(R.id.password_signup_et);
+        final EditText editTextConfirmPass = (EditText)findViewById(R.id.confirmPassword_et);
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = editTextName.getText().toString();
+                String s2 = editTextPhone.getText().toString();
+                String s3 = editTextUsername.getText().toString();
+                String s4 = editTextPassword.getText().toString();
+                String s5 = editTextConfirmPass.getText().toString();
+
+                if(s1.equals("") || s2.equals("") || s3.equals("") || s4.equals("") || s5.equals("")){
+                    Toast.makeText(getApplicationContext(),"Please, insert data!",Toast.LENGTH_LONG).show();
+                }
+
+                if(!s4.equals(s5)){
+                    Toast.makeText(getApplicationContext(),"Passwords don't match!",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
     }
 }
